ad3345d8c563b33d6c9d4627adfab45086c131f2
amuenz <annemunzner@gmail.com>
2018-09-09 18:14:28 +0200

    small fix wiki text

--- app/src/main/java/de/cdvost/jibjib/repository/converter/WikiTextCleaner.java
+++ app/src/main/java/de/cdvost/jibjib/repository/converter/WikiTextCleaner.java
@@ -4,6 +4,7 @@ public class WikiTextCleaner {
 
     public static String cleanData(String wikiText){
         String cleanedText = wikiText;
+        cleanedText = cleanedText.replaceAll("(===)([^=]*)(===)", "--$2--");
         cleanedText = cleanedText.replaceAll("(==)([^=]*)(==)", "--$2--");
         cleanedText = cleanedText.replace("\\n", System.getProperty("line.separator"));
         return cleanedText;
